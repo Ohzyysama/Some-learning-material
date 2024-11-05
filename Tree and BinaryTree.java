1173127f903d6cc03ab7a66326752afe86077c32
Ohzyysama <145254930+Ohzyysama@users.noreply.github.com>
2024-11-05 21:41:45 +0800

    Create Tree and BinaryTree.java

---
+++ Tree and BinaryTree.java
@@ -0,0 +1,12 @@
+节点类
+public class BinaryNode(){
+  private int element;
+  private BinaryNode left;
+  private BinaryNode right;
+  public BinaryNode(int element, BinaryNode left, BinaryNode right){
+    this.element = element;
+    this.left = left;
+    this.right = right;
+  }
+}
+
