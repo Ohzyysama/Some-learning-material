cd0b4183c1039da8fcef08f654c53918e7ff591b
Chou <145254930+Ohzyysama@users.noreply.github.com>
2024-11-05 21:54:37 +0800

    Update Tree and BinaryTree.java

--- Tree and BinaryTree.java
+++ Tree and BinaryTree.java
@@ -34,3 +34,82 @@ public void postorder(BinaryNode root){
   System.out.print(root.element);
   }
 }
+建立一棵二叉树，并输出前序、中序、后序遍历结果。
+public class BinTree {
+    private char element;
+    BinTree left, right;
+  
+    public BinTree() {
+    }
+  
+    BinTree(char element, BinTree left, BinTree right) {
+        this.element = element;
+        this.left = left;
+        this.right = right;
+    }
+  
+    public static BinTree CreateBT(char[] pres, char[] ins){
+        BinTree root = new BinTree();
+        root = CreateBT(0, ins.length-1, 0, pres.length-1, ins, pres);
+        return root;
+    }
+
+    public static BinTree CreateBT(int startin, int endin, int startpre, int endpre, char [] ins, char [] pres){
+        if(startin > endin || startpre > endpre){
+            return null;
+        }
+        BinTree root = new BinTree();
+        root.element = pres[startpre];
+
+        for(int i = startin ; i <= endin ; i ++){
+            if(ins[i] == pres[startpre]){
+                root.left = CreateBT(startin, i-1, startpre+1, startpre+(i-startin), ins, pres);
+                root.right = CreateBT(i+1, endin, startpre+(i-startin)+1, endpre, ins, pres);
+            }
+        }
+        return root;
+    }
+  
+    public static StringBuilder PreOrderTraversal(BinTree root) {
+        StringBuilder result = new StringBuilder();
+        if(root != null){
+            result.append(root.element);
+            if(root.left != null){
+                result.append(PreOrderTraversal(root.left)) ;
+            }
+            if(root.right != null){
+                result.append(PreOrderTraversal(root.right)) ;
+            }
+        }
+        return result;
+    }
+  
+    public static StringBuilder InOrderTraversal(BinTree root) {
+        StringBuilder result = new StringBuilder();
+        if(root != null){
+            if(root.left != null){
+                result.append(InOrderTraversal(root.left));
+            }
+            result.append(root.element);
+            if(root.right != null){
+                result.append(InOrderTraversal(root.right));
+            }
+        }
+        return result;
+    }
+  
+    public static StringBuilder PostOrderTraversal(BinTree root) {
+        StringBuilder result = new StringBuilder();
+        if(root != null){
+            if(root.left != null){
+                result.append(PostOrderTraversal(root.left));
+            }
+            if(root.right != null){
+                result.append(PostOrderTraversal(root.right));
+            }
+            result.append(root.element);
+        }
+        return result;
+    }
+
+}
